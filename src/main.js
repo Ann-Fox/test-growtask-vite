46e21a343d6153c18a78563a80185e91936f68d0
Anna Chernysheva <fx8565@mail.ru>
2024-12-18 14:57:15 +0500

    feat: replace px -> vw (Header)

--- src/main.js
+++ src/main.js
@@ -39,7 +39,6 @@ presentationBtns.forEach((el, index) => {
 
 //  Section FAQ
 const faqWrapper = document.querySelectorAll('.faq__item')
-console.log(faqWrapper)
 
 document.addEventListener('DOMContentLoaded', () => {
     const items = document.querySelectorAll('.faq__item');
@@ -67,14 +66,3 @@ if (btnEl) {
         btnEl.classList.toggle('active')
     })
 }
-
-// function validateEmail(email) {
-//     let re =
-//         /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//     return re.test(String(email).toLowerCase());
-// }
-//
-// function validatePhone(phone) {
-//     let regex = /^(\+7)?[\s]?\(?[0-9]{3}\)?[\s]?[0-9]{3}[\s]?[0-9]{4}$/;
-//     return regex.test(phone);
-// }
